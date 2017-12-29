1ae253220c5e19700e92d72b53a241fbc5605363
Jason Dixon <jason.dixon.email@gmail.com>
2017-12-30 00:07:22 +1300

    adding file size to hash

--- content/main.go
+++ content/main.go
@@ -6,16 +6,13 @@ import (
   "os"
   "io"
   "log"
-  // "errors"
   "hash"
   "encoding/hex"
   "crypto/sha256"
   "crypto/sha1"
+  "strconv"
 )
 
-
-
-var PATH = "D:/Documents/go-workspace/src/github.com/internetimagery/photos/test.jpg"
 
 // Return a hash representing the files content
 func Hash(hash_type, path string) string {
@@ -26,27 +23,36 @@ func Hash(hash_type, path string) string {
   }
   defer f.Close()
 
+  // Get file stats
+  stat, err := f.Stat()
+  if err != nil {
+    log.Panic(err)
+  }
+  size := strconv.FormatInt(stat.Size(), 10)
+
   // Choose hash
   var h hash.Hash
-
   switch hash_type {
     case "SHA1":
       h = sha1.New()
     case "SHA256":
       h = sha256.New()
     default:
-      log.Fatalf("Hash type undefined %s", hash_type)
+      log.Fatalf("Hash type undefined: %s", hash_type)
   }
 
   // Generate hash
   if _, err := io.Copy(h, f); err != nil {
     log.Panic(err)
   }
+  fingerprint := hex.EncodeToString(h.Sum(nil))
 
   // Return hash value
-  return hex.EncodeToString(h.Sum(nil))
+  return size + "-" + fingerprint
 }
 
+
 func main()  {
-  fmt.Println(Hash("SHA1", PATH))
+  p := "D:/Documents/go-workspace/src/github.com/internetimagery/photos/test.jpg"
+  fmt.Println(Hash("SHA1", p))
 }
