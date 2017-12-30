90df9320b7cc85bde65e73d8d66257aa5158fd7c
Jason Dixon <jason.dixon.email@gmail.com>
2017-12-30 23:58:47 +1300

    format

--- config/main.go
+++ config/main.go
@@ -2,34 +2,33 @@
 package config
 
 import (
-  "log"
-  "io/ioutil"
-  "github.com/internetimagery/photos/utility"
+	"github.com/internetimagery/photos/utility"
+	"io/ioutil"
+	"log"
 )
 
 const CONFIGNAME = "photos_conf.json"
 
 type Config struct {
-  UUID, Name string
+	UUID, Name string
 }
 
 // Find config in the current context
 func GetConfig() string {
-  cwd := utility.CWD()
-  return utility.SearchUp(CONFIGNAME, cwd)
+	cwd := utility.CWD()
+	return utility.SearchUp(CONFIGNAME, cwd)
 }
 
 // Create new config
 func NewConfig(name string) *Config {
-  conf := &Config{UUID: utility.GenerateID(), Name: name}
-  return conf
+	conf := &Config{UUID: utility.GenerateID(), Name: name}
+	return conf
 }
 
 // Read a config
 func LoadConfig(path string) *Config {
-  
-}
 
+}
 
 // cwd := utility.CWD()
 // files := ioutil.ReadDir(cwd)
--- main.go
+++ main.go
@@ -1,31 +1,34 @@
 package main
 
 import (
-  "github.com/internetimagery/photos/rename"
-  "strings"
-  "fmt"
-  "os"
+	"fmt"
+	"github.com/internetimagery/photos/rename"
+	"os"
+	"strings"
 )
 
 func help() {
-  fmt.Println("Shrink, Rename, Backup photos!")
-  fmt.Println("photos COMMAND ARGS")
-  fmt.Println("(WIP) INIT   :: Set up the root of your photo project.")
-  fmt.Println("(WIP) CONFIG :: Project settings")
-  fmt.Println("(WIP) PROCESS:: Compress and rename photos.")
-  fmt.Println("(WIP) BACKUP :: Copy files to another location.")
-  fmt.Println("(WIP) DROP   :: Remove file from project, replacing with a pointer to original.")
-  fmt.Println("(WIP) GET    :: Retrieve dropped file.")
+	fmt.Println("Shrink, Rename, Backup photos!")
+	fmt.Println("photos COMMAND ARGS")
+	fmt.Println("(WIP) INIT   :: Set up the root of your photo project.")
+	fmt.Println("(WIP) CONFIG :: Project settings")
+	fmt.Println("(WIP) PROCESS:: Compress and rename photos.")
+	fmt.Println("(WIP) BACKUP :: Copy files to another location.")
+	fmt.Println("(WIP) DROP   :: Remove file from project, replacing with a pointer to original.")
+	fmt.Println("(WIP) GET    :: Retrieve dropped file.")
 }
 
-func main()  {
-  if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
-    help()
-  } else {
-    switch strings.ToLower(os.Args[1]) {
-      case "init": rename.Run(os.Args[2:])
-      case "rename": rename.Run(os.Args[2:])
-      default: help()
-    }
-  }
+func main() {
+	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		help()
+	} else {
+		switch strings.ToLower(os.Args[1]) {
+		case "init":
+			rename.Run(os.Args[2:])
+		case "rename":
+			rename.Run(os.Args[2:])
+		default:
+			help()
+		}
+	}
 }
