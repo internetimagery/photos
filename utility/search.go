876aaa28f04de0ef4cce39d4932755f93225b4a5
Jason Dixon <jason.dixon.email@gmail.com>
2017-12-30 19:19:19 +1300

    searching

---
+++ utility/search.go
@@ -0,0 +1,31 @@
+// walk file path
+package main
+
+import (
+  "fmt"
+  "os"
+  "io/ioutil"
+  "path/filepath"
+)
+
+func SearchUp(search, root string) string {
+  for true {
+    files, _ := ioutil.ReadDir(root)
+    for i := 0; i < len(files); i++ {
+      name := files[i].Name()
+      if search == name {
+        return filepath.Join(root, name)
+      }
+    }
+    new_root := filepath.Dir(root)
+    if new_root == root {
+      break
+    }
+    root = new_root
+  }
+  return ""
+}
+
+func main()  {
+  fmt.Println(SearchUp("Go", os.Args[0]))
+}
