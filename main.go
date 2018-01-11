1dc1517454456ab0a8575b248e8115f517b07681
Jason Dixon <jason.dixon.email@gmail.com>
2018-01-12 07:52:27 +1300

    stuff

--- main.go
+++ main.go
@@ -11,54 +11,82 @@ import (
 	"strings"
 )
 
-type run func([]string, *config.Config)
-
-var ARGS = map[string]run{
-	"init": cmdinit.Run,
-	"config": cmdconfig.Run,
+type Module interface {
+	// Execute module
+	Run([]string, *config.Config) int
+	// Return short module description
+	Desc() string
 }
 
-func help() {
-	fmt.Println("Shrink, Rename, Backup photos!")
-	fmt.Println(">>>photos COMMAND ARGS")
-	fmt.Println("(WIP) INIT   :: Set up the root of your photo project.")
-	fmt.Println("(WIP) CONFIG :: Project settings")
-	fmt.Println("(WIP) ADD    :: Compress and rename photos.")
-	fmt.Println("(WIP) BACKUP :: Copy files to another location.")
-	fmt.Println("(WIP) DROP   :: Remove file from project, replacing with a pointer to original.")
-	fmt.Println("(WIP) GET    :: Retrieve dropped file.")
+var mod = map[string]Module{
+	"init": cmdinit
 }
 
-func main() {
-	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
-		help()
-	} else {
-		arg := strings.ToLower(os.Args[1])
-		cwd, err := os.Getwd()
-		if err != nil {
-			log.Panic(err)
-		}
-		conf := config.GetConfig(cwd)
-		if arg == "init" {
-			cmdinit.Run(conf, conf)
-		}
-		if val, ok := ARGS[arg]; ok {
-			if conf.GetRoot() != "" || arg == "init" {
-				val(os.Args[2:], conf)
-				return
-			}
-			log.Panic("Not inside repository")
+func help(mod map[string]Module)  {
+
+}
+
+func main()  {
+	if len(os.Args) == 0 {
+		help(mod)
+	}
+	fnc, ok := mod[os.Args[1]]
+		if ok != nil {
+			continue
 		} else {
-			options := make([]string, len(ARGS))
-			i := 0
-			for k := range ARGS {
-				options[i] = k
-				i++
-			}
-			guess := utility.ClosestMatch(arg, options)
-			fmt.Printf("Argument \"%s\" does not exist.\n", arg)
-			fmt.Println("Did you mean:")
-			fmt.Printf("\t%s", guess)
+			os.Exit(fnc(os.Args[1:]))
 		}
 	}
 }
+//
+// type run func([]string, *config.Config)
+//
+// var ARGS = map[string]run{
+// 	"init": cmdinit.Run,
+// 	"config": cmdconfig.Run,
+// }
+//
+// func help() {
+// 	fmt.Println("Shrink, Rename, Backup photos!")
+// 	fmt.Println(">>>photos COMMAND ARGS")
+// 	fmt.Println("(WIP) INIT   :: Set up the root of your photo project.")
+// 	fmt.Println("(WIP) CONFIG :: Project settings")
+// 	fmt.Println("(WIP) ADD    :: Compress and rename photos.")
+// 	fmt.Println("(WIP) BACKUP :: Copy files to another location.")
+// 	fmt.Println("(WIP) DROP   :: Remove file from project, replacing with a pointer to original.")
+// 	fmt.Println("(WIP) GET    :: Retrieve dropped file.")
+// }
+//
+// func main() {
+// 	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+// 		help()
+// 	} else {
+// 		arg := strings.ToLower(os.Args[1])
+// 		cwd, err := os.Getwd()
+// 		if err != nil {
+// 			log.Panic(err)
+// 		}
+// 		conf := config.GetConfig(cwd)
+// 		if arg == "init" {
+// 			cmdinit.Run(conf, conf)
+// 		}
+// 		if val, ok := ARGS[arg]; ok {
+// 			if conf.GetRoot() != "" || arg == "init" {
+// 				val(os.Args[2:], conf)
+// 				return
+// 			}
+// 			log.Panic("Not inside repository")
+// 		} else {
+// 			options := make([]string, len(ARGS))
+// 			i := 0
+// 			for k := range ARGS {
+// 				options[i] = k
+// 				i++
+// 			}
+// 			guess := utility.ClosestMatch(arg, options)
+// 			fmt.Printf("Argument \"%s\" does not exist.\n", arg)
+// 			fmt.Println("Did you mean:")
+// 			fmt.Printf("\t%s", guess)
+// 		}
+// 	}
+// }
