8c3cec61f1ec849147cc26f931efb5e765e740c8
Jason Dixon <jason.dixon.email@gmail.com>
2018-01-21 23:13:29 +1300

    adding state

--- state/state.go
+++ state/state.go
@@ -1,17 +1,36 @@
 package state
 
 import (
-  "fmt"
-  "github.com/internetimagery/photos/state"
-  "github.com/internetimagery/photos/config"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/internetimagery/photos/config"
+	"github.com/internetimagery/photos/utility"
 )
 
+const CONFIG = "test_config.json"
+
+// Manage basic cli state
 type State struct {
-  Root, Cwd string
-  Conf *config.Config
+	Root, Cwd, Conf_Path string
+	Conf                 *config.Config
 }
 
-func (_ State) New(cwd string) *State {
-  res := new(State)
-  res.Cwd = cwd
+func (_ State) New() *State {
+	// Get current directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Panic(err)
+	}
+	// Look for config file
+	conf := utility.SearchUp(CONFIG, cwd)
+	root := filepath.Dir(conf)
+
+	// TODO: Collect config path and file etc
+	state := new(State)
+	state.Cwd = cwd
+	state.Conf_Path = conf
+	state.Root = root
+	return state
 }
