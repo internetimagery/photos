32fb55c809275741bcc177036dd7840224a08731
Jason Dixon <jason.dixon.email@gmail.com>
2018-02-04 13:25:42 +1300

    adding config tests

--- config/main.go
+++ config/main.go
@@ -2,86 +2,124 @@
 package config
 
 import (
-	"github.com/internetimagery/photos/utility"
+	"encoding/json"
 	"io/ioutil"
-	"log"
-  "encoding/json"
-  "path/filepath"
-  // "fmt"
+
+	uuid "github.com/satori/go.uuid"
 )
 
-const CONFIGNAME = "photos_conf.json"
+// "fmt"
 
-// REMOTE!
-// UUID: Unique identifier (taken from repo config)
-// Name: Friendly name (from repo config)
-// Locations: List of prefixes that leads to repo
-type Remote struct {
-  UUID, Name string
-  Locations []string
+// RandomID from mac address and time
+func GenerateID() string {
+	return uuid.NewV4().String()
 }
 
-// CONFIG!
-// UUID: Unique identifier
-// Name: Friendly Name
-// Rclone: rclone location (command)
-// Bin: (optional) path to rclone
-// Remotes: remote repos
 type Config struct {
-	UUID, Name, Rclone, root string
-  Remotes map[string]*Remote
-}
-func (self Config) GetRoot() string {
-  return self.root
+	ID, Name, Root string
 }
 
-// Find config in the current context
-func GetConfig(root string) *Config {
-  var conf Config
-  pconf := &conf
-  path := utility.SearchUp(CONFIGNAME, root)
-  if path != "" {
-    pconf = LoadConfig(path)
-  }
-  return pconf
-}
-
-// Create new config
+// Create a new config file
 func NewConfig() *Config {
-	conf := &Config{UUID: utility.GenerateID()}
+	// Perform initial setup here
+	conf := new(Config)
+	conf.ID = GenerateID()
 	return conf
 }
 
-// Save config file to disk
-func SaveConfig(conf *Config, path string)  {
-  data, err := json.MarshalIndent(conf, "", "  ")
-  if err != nil {
-    log.Panic(err)
-  }
-  err = ioutil.WriteFile(path, data, 644)
-  if err != nil {
-    log.Panic(err)
-  }
+func LoadConfig(path string) (*Config, error) {
+	conf := new(Config)
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return conf, err
+	}
+	err = json.Unmarshal(data, conf)
+	conf.Root = path
+	return conf, err
 }
 
-// Read a config from file
-func LoadConfig(path string) *Config {
-  data, err := ioutil.ReadFile(path)
-  if err != nil {
-    log.Panic(err)
-  }
-  conf := &Config{root:filepath.Dir(path)}
-  err = json.Unmarshal(data, conf)
-  if err != nil {
-    log.Panic(err)
-  }
-  return conf
+func (self Config) Save(path string) error {
+	data, err := json.MarshalIndent(self, "", "  ")
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(path, data, 644)
+	return err
 }
 
-// cwd := utility.CWD()
-// files := ioutil.ReadDir(cwd)
-// for i := 0; i < len(files); i++ {
-//   if files[i].Name() == CONFIGNAME {
-//     log.Fatal("Config file already exists.")
+//
+// const CONFIGNAME = "photos_conf.json"
+//
+// // REMOTE!
+// // UUID: Unique identifier (taken from repo config)
+// // Name: Friendly name (from repo config)
+// // Locations: List of prefixes that leads to repo
+// type Remote struct {
+//   UUID, Name string
+//   Locations []string
+// }
+//
+// // CONFIG!
+// // UUID: Unique identifier
+// // Name: Friendly Name
+// // Rclone: rclone location (command)
+// // Bin: (optional) path to rclone
+// // Remotes: remote repos
+// type Config struct {
+// 	UUID, Name, Rclone, root string
+//   Remotes map[string]*Remote
+// }
+// func (self Config) GetRoot() string {
+//   return self.root
+// }
+//
+// // Find config in the current context
+// func GetConfig(root string) *Config {
+//   var conf Config
+//   pconf := &conf
+//   path := utility.SearchUp(CONFIGNAME, root)
+//   if path != "" {
+//     pconf = LoadConfig(path)
+//   }
+//   return pconf
+// }
+//
+// // Create new config
+// func NewConfig() *Config {
+// 	conf := &Config{UUID: utility.GenerateID()}
+// 	return conf
+// }
+//
+// // Save config file to disk
+// func SaveConfig(conf *Config, path string)  {
+//   data, err := json.MarshalIndent(conf, "", "  ")
+//   if err != nil {
+//     log.Panic(err)
+//   }
+//   err = ioutil.WriteFile(path, data, 644)
+//   if err != nil {
+//     log.Panic(err)
 //   }
 // }
+//
+// // Read a config from file
+// func LoadConfig(path string) *Config {
+//   data, err := ioutil.ReadFile(path)
+//   if err != nil {
+//     log.Panic(err)
+//   }
+//   conf := &Config{root:filepath.Dir(path)}
+//   err = json.Unmarshal(data, conf)
+//   if err != nil {
+//     log.Panic(err)
+//   }
+//   return conf
+// }
+//
+// // cwd := utility.CWD()
+// // files := ioutil.ReadDir(cwd)
+// // for i := 0; i < len(files); i++ {
+// //   if files[i].Name() == CONFIGNAME {
+// //     log.Fatal("Config file already exists.")
+// //   }
+// // }
---
+++ config/main_test.go
@@ -0,0 +1,109 @@
+// Testing Config file.
+package config
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+type Temp struct {
+	Name string
+}
+
+func (self Temp) Remove() {
+	os.RemoveAll(self.Name)
+}
+
+func (self Temp) File(name string) string {
+	return filepath.Join(self.Name, name)
+}
+
+func (self Temp) Missing(path string) bool {
+	_, err := os.Stat(path)
+	return os.IsNotExist(err)
+}
+
+func NewTemp() (*Temp, error) {
+	tmp := new(Temp)
+	dir, err := ioutil.TempDir("", "Go_Test")
+	tmp.Name = dir
+	return tmp, err
+}
+
+func TestNew(t *testing.T) {
+	config1 := NewConfig()
+	config2 := NewConfig()
+	if config1.ID == config2.ID {
+		t.Fail()
+	}
+}
+
+func TestSave(t *testing.T) {
+	dir, err := NewTemp()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dir.Remove()
+
+	config := NewConfig()
+	tmp := dir.File("config.json")
+	err = config.Save(tmp)
+	if err != nil {
+		t.Error(err)
+	}
+	if dir.Missing(tmp) {
+		t.Fail()
+	}
+}
+
+func TestLoad(t *testing.T) {
+	dir, err := NewTemp()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dir.Remove()
+
+	config := NewConfig()
+	tmp := dir.File("config.json")
+	err = config.Save(tmp)
+	if err != nil {
+		t.Error(err)
+	}
+	config2, err := LoadConfig(tmp)
+	if err != nil {
+		t.Error(err)
+	}
+	if config.ID != config2.ID {
+		t.Fail()
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	dir, err := NewTemp()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dir.Remove()
+
+	config := NewConfig()
+	tmp := dir.File("config.json")
+	err = config.Save(tmp)
+	if err != nil {
+		t.Error(err)
+	}
+	name := "TEST123"
+	config.Name = name
+	err = config.Save(tmp)
+	if err != nil {
+		t.Error(err)
+	}
+	config2, err := LoadConfig(tmp)
+	if err != nil {
+		t.Error(err)
+	}
+	if config.Name != config2.Name {
+		t.Fail()
+	}
+}
