0a7f22d2591a4885d42638ea08c278c79068a0c5
Jason Dixon <jason.dixon.email@gmail.com>
2018-02-06 00:16:08 +1300

    oneliner!

--- format/main_test.go
+++ format/main_test.go
@@ -10,6 +10,7 @@ func TestMatch(t *testing.T) {
 	bad2, _ := Match("dir_name", []string{"whatever_002.png"})
 	bad3, _ := Match("dir_name", []string{"dir_name[tags].png"})
 	bad4, _ := Match("dir_name", []string{"dir_name_004{why not}.png"})
+	bad5, _ := Match("dir_name", []string{"dir_name_005[why yes.png"})
 
 	if !good1[0].Formatted || good1[0].Index != 1 {
 		t.Fail()
@@ -17,16 +18,7 @@ func TestMatch(t *testing.T) {
 	if !good2[0].Formatted || good2[0].Index != 1 || good2[0].Tags[0] != "stuff" {
 		t.Fail()
 	}
-	if bad1[0].Formatted {
-		t.Fail()
-	}
-	if bad2[0].Formatted {
-		t.Fail()
-	}
-	if bad3[0].Formatted {
-		t.Fail()
-	}
-	if bad4[0].Formatted {
+	if bad1[0].Formatted || bad2[0].Formatted || bad3[0].Formatted || bad4[0].Formatted || bad5[0].Formatted {
 		t.Fail()
 	}
 }
