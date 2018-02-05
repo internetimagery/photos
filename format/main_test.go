599f48b99cf2f2c3fa0d1ddfae1ec8fecc806898
Jason Dixon <jason.dixon.email@gmail.com>
2018-02-06 01:08:43 +1300

    simplify

--- format/main_test.go
+++ format/main_test.go
@@ -6,22 +6,28 @@ import (
 )
 
 func TestMatch(t *testing.T) {
-	good1, _ := Match("dir_name", []string{"dir_name_001.mov"})
-	good2, _ := Match("dir_name", []string{"dir_name_001[stuff more stuff].jpg"})
-	bad1, _ := Match("dir_name", []string{"whatever.png"})
-	bad2, _ := Match("dir_name", []string{"whatever_002.png"})
-	bad3, _ := Match("dir_name", []string{"dir_name[tags].png"})
-	bad4, _ := Match("dir_name", []string{"dir_name_004{why not}.png"})
-	bad5, _ := Match("dir_name", []string{"dir_name_005[why yes.png"})
+	good, _ := Match("dir_name", []string{
+		"dir_name_001.mov",
+		"dir_name_001[stuff more stuff].jpg",
+	})
+	bad, _ := Match("dir_name", []string{
+		"whatever.png",
+		"whatever_002.png",
+		"dir_name[tags].png",
+		"dir_name_004{why not}.png",
+		"dir_name_005[why yes.png",
+	})
 
-	if !good1[0].Formatted || good1[0].Index != 1 {
+	if !good[0].Formatted || good[0].Index != 1 {
 		t.Fail()
 	}
-	if !good2[0].Formatted || good2[0].Index != 1 || good2[0].Tags[0] != "stuff" {
+	if !good[1].Formatted || good[1].Index != 1 || good[1].Tags[0] != "stuff" {
 		t.Fail()
 	}
-	if bad1[0].Formatted || bad2[0].Formatted || bad3[0].Formatted || bad4[0].Formatted || bad5[0].Formatted {
-		t.Fail()
+	for _, b := range bad {
+		if b.Formatted {
+			t.Fail()
+		}
 	}
 }
 
