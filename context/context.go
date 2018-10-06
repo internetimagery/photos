efba4b34f97f7fd802424ac9368cf2130c2d91c9
Jason Dixon <jason.dixon.email@gmail.com>
2018-10-07 00:13:39 +1300

    context seems to be working ok

--- context/context.go
+++ context/context.go
@@ -1,24 +1,47 @@
 package context
 
-import "github.com/derekparker/delve/pkg/config"
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/internetimagery/photos/config"
+)
 
 // ROOTCONF : name of config file that marks the root of the project (as well as important information)
 const ROOTCONF = "photos.config"
 
 // Context : Collect and encapsulate information about project
 type Context struct {
-	Root             string        // Path to base of repository (location of config)
-	WorkingDirectory string        // Path of current working directory
-	Config           config.Config // Configuration information
+	Root       string         // Path to base of repository (location of config)
+	WorkingDir string         // Path of current working directory
+	Config     *config.Config // Configuration information
 }
 
 // NewContext : Create a new context, gathering information
-func NewContext(workingDirectory string) (*Context, error) {
-	// TODO: search up the path from workingDirectory to find config file
-	return nil, nil
-}
+func NewContext(workingDir string) (*Context, error) {
+	// Walk our way up till we find a config file denoting the project root
+	currentRoot := workingDir
+	configPath := ""
+	for {
+		configPath = filepath.Join(currentRoot, ROOTCONF) // Check if config file exists
+		if _, err := os.Stat(configPath); err == nil {
+			break
+		}
+		if currentRoot[len(currentRoot)-1] == filepath.Separator { // Check if we have hit root finding nothing, leave if so
+			return nil, os.ErrNotExist
+		}
+		currentRoot = filepath.Dir(currentRoot)
+	}
 
-// GetRelativePath : Make given path relative to project root. Error if not in project...
-func GetRelativePath(path string) (string, error) {
-	return "", nil
+	// Attempt to load the config file
+	handle, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer handle.Close()
+	conf, err := config.LoadConfig(handle)
+	if err != nil {
+		return nil, err
+	}
+	return &Context{Root: currentRoot, WorkingDir: workingDir, Config: conf}, nil
 }
--- context/context_test.go
+++ context/context_test.go
@@ -2,13 +2,39 @@ package context
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/internetimagery/photos/sandbox"
 )
 
+func TestNewContext(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "photostest")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDir)
+	if _, err := NewContext(tmpDir); !os.IsNotExist(err) {
+		fmt.Println(err)
+		t.Fail()
+	}
+}
+
 func TestContext(t *testing.T) {
 	sb := sandbox.NewSandBox(t)
 	defer sb.Close()
-	fmt.Println(sb)
+
+	// Start within event directory
+	cxt, err := NewContext(sb.Join("event01"))
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	// Check root and workingDir are not the same!
+	if cxt.Root == cxt.WorkingDir {
+		fmt.Println("Root is the same as workingDir!")
+		t.Fail()
+	}
 }
