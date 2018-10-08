e703d534efec9e2563697e7b5ad622dfa8950891
Jason Dixon <jason.dixon.email@gmail.com>
2018-10-08 22:46:55 +1300

    some formats

--- format/format.go
+++ format/format.go
@@ -2,14 +2,17 @@ package format
 
 import (
 	"fmt"
+	"path/filepath"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
-var eventReg = `(%s)`
-var indexReg = `_(\d+)`
-var tagReg = `[\w\-_ ]+`
-var tagsReg = fmt.Sprintf(`(?:\[(%s)\])?`, tagReg)
-var extReg = `\.(\w+)`
+var eventReg = `[\w\-_ ]+` // Valid event
+var indexReg = `\d+`       // Valid Index
+var tagReg = `[\w\-_ ]+`   // Valid Tags
+var extReg = `\w+`
+var formatReg = regexp.MustCompile(fmt.Sprintf(`(%s)_(%s)(?:\[(%s)\])?\.(%s)`, eventReg, indexReg, tagReg, extReg))
 
 // Media : Container for information about media item
 type Media struct {
@@ -20,9 +23,45 @@ type Media struct {
 	Ext   string   // Extension / file type
 }
 
-// RegFromEvent : Form regex from event name
-func RegFromEvent(event string) *regexp.Regexp {
-	return regexp.MustCompile(
-		fmt.Sprintf(eventReg, regexp.QuoteMeta(event)) +
-			indexReg + tagsReg + extReg)
+// NewMedia : Create new media representation
+func NewMedia(filename string) *Media {
+	media := new(Media)
+	media.Name = filename
+	media.Ext = filepath.Ext(filename)[1:]
+	parts := formatReg.FindStringSubmatch(filename)
+	if len(parts) > 0 {
+		media.Event = parts[1]
+		index, _ := strconv.Atoi(parts[2])
+		media.Index = index
+		if len(parts[3]) > 0 {
+			media.Tags = strings.Split(parts[3], " ")
+		}
+	}
+	return media
+}
+
+// FormatName : Given the current settings (which may have been modified), validate and format a corresponding name.
+func (media *Media) FormatName() (string, error) {
+	// Validate our inputs
+	if !regexp.MustCompile(eventReg).MatchString(media.Event) || strings.TrimSpace(media.Event) == "" {
+		return "", fmt.Errorf("Bad Event: '%s'", media.Event)
+	}
+	if media.Index <= 0 {
+		return "", fmt.Errorf("Index value too low: '%d'", media.Index)
+	}
+	if !regexp.MustCompile(extReg).MatchString(media.Ext) || strings.TrimSpace(media.Ext) == "" {
+		return "", fmt.Errorf("Bad extension: '%s'", media.Ext)
+	}
+	tagTest := regexp.MustCompile(tagReg)
+	for _, tag := range media.Tags {
+		if !tagTest.MatchString(tag) || strings.TrimSpace(tag) == "" {
+			return "", fmt.Errorf("Bad tag: '%s'", tag)
+		}
+	}
+
+	tags := ""
+	if len(media.Tags) > 0 {
+		tags = fmt.Sprintf("[%s]", strings.Join(media.Tags, " "))
+	}
+	return fmt.Sprintf("%s_%03d%s.%s", media.Event, media.Index, tags, media.Ext), nil
 }
--- format/format_test.go
+++ format/format_test.go
@@ -5,20 +5,52 @@ import (
 	"testing"
 )
 
-func TestRegexFromEvent(t *testing.T) {
+func TestNewMedia(t *testing.T) {
 	event := "18-12-08 event"
-	tests := map[string]bool{
-		"18-12-08 event_0032[someone something].jpg": true,
-		"18-12-08 event_0032.png":                    true,
-		"18-12-08 event023.png":                      false,
-		"_023.png":                                   false,
-		"18-12-08 event_0032[a thing.png":            false,
+
+	// Test filename with tags
+	test1 := event + "_002[one-two three].jpg"
+	media1 := NewMedia(test1)
+	if media1.Event != event || media1.Index != 2 || media1.Name != test1 || media1.Ext != "jpg" || len(media1.Tags) != 2 {
+		fmt.Println("Failed on", test1)
+		fmt.Println(media1)
+		t.Fail()
 	}
 
-	reg := RegFromEvent(event)
-	for test, match := range tests {
-		if match != reg.MatchString(test) {
-			fmt.Println("Failed on", test)
+	// Test filename without tags
+	test2 := event + "_202.png"
+	media2 := NewMedia(test2)
+	if media2.Event != event || media2.Index != 202 || media2.Name != test2 || media2.Ext != "png" || len(media2.Tags) != 0 {
+		fmt.Println("Failed on", test2)
+		fmt.Println(media2)
+		t.Fail()
+	}
+
+	// Test filename unformatted
+	test3 := "my_fav_picture.jpeg"
+	media3 := NewMedia(test3)
+	if media3.Index != 0 {
+		fmt.Println("Failed on", test3)
+		fmt.Println(media3)
+		t.Fail()
+	}
+
+}
+
+func TestFormatName(t *testing.T) {
+	tests := make(map[string]Media)
+
+	tests["event01_020.png"] = Media{Event: "event01", Index: 20, Ext: "png"}
+	tests["18-12-07 event_1234[one two].jpeg"] = Media{Event: "18-12-07 event", Index: 1234, Tags: []string{"one", "two"}, Ext: "jpeg"}
+	tests[""] = Media{Event: "some event/event", Index: 0, Ext: "$$$"}
+
+	for expect, test := range tests {
+		result, err := test.FormatName()
+		if err != nil && expect != "" {
+			fmt.Println(err)
+			t.Fail()
+		} else if result != expect {
+			fmt.Printf("Expected '%s', got '%s'\n", expect, result)
 			t.Fail()
 		}
 	}
