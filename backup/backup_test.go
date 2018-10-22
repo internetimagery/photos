133e1435ffa2169b3b566c9b851766c2f039d6e5
Jason Dixon <jason.dixon.email@gmail.com>
2018-10-22 22:19:09 +1300

    adding backup stuff

--- backup/backup_test.go
+++ backup/backup_test.go
@@ -4,62 +4,49 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/internetimagery/photos/config"
 	"github.com/internetimagery/photos/context"
 	"github.com/internetimagery/photos/testutil"
 )
 
 func TestRunBackup(t *testing.T) {
 	tu := testutil.NewTestUtil(t)
-	defer tu.TempDir("TestRunBackup")()
+	defer tu.LoadTestdata()()
 
-	testFile1 := filepath.Join(tu.Dir, "testfile1.txt")
-	testFile2 := filepath.Join(tu.Dir, "testfile2.txt")
-
-	cxt := &context.Context{
-		Env: map[string]string{
-			"TESTPATH1": testFile1,
-			"TESTPATH2": testFile2,
-		},
-		Root:       tu.Dir,
-		WorkingDir: tu.Dir,
-		Config: &config.Config{
-			Backup: config.BackupCategory{
-				config.Command{"test", "touch $TESTPATH1"},
-				config.Command{"other", "touch $TESTPATH2"},
-			},
-		},
+	event := filepath.Join(tu.Dir, "event01")
+	cxt, err := context.NewContext(event)
+	if err != nil {
+		tu.Fatal(err)
 	}
 
+	// Set environment
+	cxt.Env["TESTPATH1"] = filepath.Join(event, "testfile1.txt")
+	cxt.Env["TESTPATH2"] = filepath.Join(event, "testfile2.txt")
+
 	// Test missing command
-	err := RunBackup(cxt, "nocommand")
-	if err != nil {
+	if err = RunBackup(cxt, "nocommand"); err != nil {
 		tu.Fail(err)
 	}
 
 	// Test no command
-	err = RunBackup(cxt, "")
-	if err != nil {
+	if err = RunBackup(cxt, ""); err != nil {
 		tu.Fail(err)
 	}
 
 	// Test command
-	err = RunBackup(cxt, "test")
-	if err != nil {
+	if err = RunBackup(cxt, "test"); err != nil {
 		tu.Fail(err)
 	}
 
 	// File should now exist
-	tu.AssertExists(testFile1)
+	tu.AssertExists(cxt.Env["TESTFILE1"])
 
 	// Test command star
-	err = RunBackup(cxt, "othe*")
-	if err != nil {
+	if err = RunBackup(cxt, "othe*"); err != nil {
 		tu.Fail(err)
 	}
 
 	// File should now exist
-	tu.AssertExists(testFile2)
+	tu.AssertExists(cxt.Env["TESTFILE2"])
 
 }
 
