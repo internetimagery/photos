0e68e7521a4c8dc6a1f35d7426cf72df55b5a5b0
Jason Dixon <jason.dixon.email@gmail.com>
2018-10-27 10:32:28 +1300

    basic env setup

--- copy/copy_test.go
+++ copy/copy_test.go
@@ -4,31 +4,58 @@ import (
 	"bytes"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"testing"
 	"time"
 )
 
-func TestCopyFile(t *testing.T) {
+type TestEnv struct {
+	Dir string
+	t   *testing.T
+}
 
-	// Set up test environment (cannot use testutil.LoadTestdata() here)
-	tmpDir, err := ioutil.TempDir("", "TestCopyFile")
+func NewTestEnv(t *testing.T) *TestEnv {
+	tmpDir, err := ioutil.TempDir("", t.Name())
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.RemoveAll(tmpDir)
-
-	sourceFile := filepath.Join(tmpDir, "testfile1.txt")
-	destFile := filepath.Join(tmpDir, "testfile2.txt")
-	perms := os.FileMode(0640)
-	modtime := time.Date(2018, 10, 10, 0, 0, 0, 0, time.Local)
-	data := []byte(strings.Repeat("TESTING AND", 100))
-	fileSize := int64(len(data))
-	if err = ioutil.WriteFile(sourceFile, data, perms); err != nil {
+	cmd := exec.Command("cp", "avT", filepath.Join("testdata", t.Name()), tmpDir)
+	if output, err := cmd.CombinedOutput(); err != nil {
+		t.Log(string(output))
 		t.Fatal(err)
 	}
-	if err = os.Chtimes(sourceFile, modtime, modtime); err != nil {
+	return &TestEnv{Dir: tmpDir, t: t}
+}
+
+func (env *TestEnv) Close() {
+	if err := os.RemoveAll(env.Dir); err != nil {
+		env.t.Fatal(err)
+	}
+}
+
+func TestCopyFile(t *testing.T) {
+
+	// Set up test environment (cannot use testutil.LoadTestdata() here)
+	tu := NewTestEnv(t)
+	defer tu.Close()
+
+	sourceFile := filepath.Join(tu.Dir, "testfile1.txt")
+	destFile := filepath.Join(tu.Dir, "testfile2.txt")
+	perms := os.FileMode(0640)
+	modtime := time.Date(2018, 10, 10, 0, 0, 0, 0, time.Local)
+	if err := os.Chtimes(sourceFile, modtime, modtime); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.Chmod(sourceFile, perms); err != nil {
+		t.Fatal(err)
+	}
+
+	// Grab info about file
+	sourceInfo, err := os.Stat(sourceFile)
+	if err != nil {
 		t.Fatal(err)
 	}
 
@@ -38,26 +65,35 @@ func TestCopyFile(t *testing.T) {
 		t.Fail()
 	}
 
-	// Check everything matches
-	info, err := os.Stat(destFile)
+	// Check our source file remains untouched
+	sourceInfoCompare, err := os.Stat(sourceFile)
 	if err != nil {
 		t.Log(err)
 		t.FailNow()
 	}
-	if info.ModTime() != modtime {
-		t.Log("Expected", modtime)
-		t.Log("Got", info.ModTime())
+	if sourceInfoCompare.Size() != sourceInfo.Size() || sourceInfoCompare.ModTime() != modtime {
+		t.Log("Source file was modified!")
 		t.Fail()
 	}
-	if info.Size() != fileSize {
-		t.Log("File sizes vary")
+
+	// Check our destination file matches the source
+	destInfo, err := os.Stat(destFile)
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+	if destInfo.ModTime() != modtime || destInfo.Size() != sourceInfo.Size() {
+		t.Log("Expected", sourceInfo)
+		t.Log("Got", destInfo)
 		t.Fail()
 	}
-	// if runtime.GOOS != "windows" {
-	// 	if info[0].Mode().Perm() != perms {
-	// 		tu.FailE(perms, info[0].Mode().Perm())
-	// 	}
-	// }
+	if runtime.GOOS != "windows" {
+		if destInfo.Mode().Perm() != perms {
+			t.Log("Expected", perms)
+			t.Log("Got", destInfo.Mode().Perm())
+			t.Fail()
+		}
+	}
 }
 
 func TestCopyFileExisting(t *testing.T) {
@@ -73,6 +109,47 @@ func TestCopyFileExisting(t *testing.T) {
 	destFile := filepath.Join(tmpDir, "testfile2.txt")
 	sourceData := []byte(strings.Repeat("TESTING AND", 100))
 	destData := []byte(strings.Repeat("RESULT AND", 100))
+	if err = ioutil.WriteFile(sourceFile, sourceData, 0644); err != nil {
+		t.Fatal(err)
+	}
+	if err = ioutil.WriteFile(destFile, destData, 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	// Test simple file copy works
+	if err := <-File(sourceFile, destFile); !os.IsExist(err) {
+		if err == nil {
+			t.Log("No error with exsting file")
+			t.Fail()
+		} else {
+			t.Log(err)
+			t.Fail()
+		}
+	}
+
+	// Check nothing changed
+	data, err := ioutil.ReadFile(destFile)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	if !bytes.Equal(data, destData) {
+		t.Log("Data was changed!")
+		t.Fail()
+	}
+}
+
+func TestCopyFileNotFile(t *testing.T) {
+
+	// Set up test environment (cannot use testutil.LoadTestdata() here)
+	tmpDir, err := ioutil.TempDir("", "TestCopyFileNotFile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	sourceFile := filepath.Join(tmpDir, "testfile1.txt")
+	destFile := filepath.Join(tmpDir, "testfile2.txt")
 	if err = ioutil.WriteFile(sourceFile, sourceData, 0644); err != nil {
 		t.Fatal(err)
 	}
