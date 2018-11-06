8f0030710a5804b81daba21ebab3e3dd0919652e
Jason Dixon <jason.dixon.email@gmail.com>
2018-11-07 00:24:06 +1300

    build script cleanup and compress

--- build.sh
+++ build.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-# Make publish directory
-mkdir -p publish
+mkdir -p build
 
 echo "::Building Linux"
 export GOOS=linux
-go build -o publish/photos-linux -i . && echo Done!
+mkdir -p photos-linux
+go build -o photos-linux/photos -i . &&\
+tar -cvzf build/photos-linux.tar.gz photos-linux &&\
+rm -r photos-linux &&\
+echo Done!
 
 echo "::Building Windows"
 export GOOS=windows
-go build -o publish/photos-windows.exe -i . && echo Done!
+mkdir -p photos-windows
+go build -o photos-windows/photos.exe -i . &&\
+zip build/photos-windows.zip -r photos-windows &&\
+rm -r photos-windows &&\
+echo Done!
 
 echo "::Building OSX"
 export GOOS=darwin
-go build -o publish/photos-osx -i . && echo Done!
+mkdir -p photos-osx
+go build -o photos-osx/photos -i . &&\
+tar -cvzf build/photos-osx.tar.gz photos-osx &&\
+rm -r photos-osx &&\
+echo Done!
