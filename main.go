f8de7495febabc4eac61cdc87cad03952d88c953
Jason Dixon <jason.dixon.email@gmail.com>
2018-11-10 14:35:24 +1300

    autocomplete

--- main.go
+++ main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -15,6 +16,7 @@ import (
 	"github.com/internetimagery/photos/rename"
 	"github.com/internetimagery/photos/sort"
 	"github.com/internetimagery/photos/tags"
+	"github.com/posener/complete"
 )
 
 // VERSION : Version information
@@ -194,6 +196,47 @@ func run(cwd string, args []string) error {
 }
 
 func main() {
+
+	// add a variable to the program
+	var name string
+	flag.StringVar(&name, "name", "", "Give your name")
+
+	// create the complete command
+	cmp := complete.New(
+		"photos.exe",
+		complete.Command{Flags: complete.Flags{"-name": complete.PredictAnything}},
+	)
+
+	// AddFlags adds the completion flags to the program flags,
+	// in case of using non-default flag set, it is possible to pass
+	// it as an argument.
+	// it is possible to set custom flags name
+	// so when one will type 'self -h', he will see '-complete' to install the
+	// completion and -uncomplete to uninstall it.
+	cmp.CLI.InstallName = "complete"
+	cmp.CLI.UninstallName = "uncomplete"
+	cmp.AddFlags(nil)
+
+	// parse the flags - both the program's flags and the completion flags
+	flag.Parse()
+
+	// run the completion, in case that the completion was invoked
+	// and ran as a completion script or handled a flag that passed
+	// as argument, the Run method will return true,
+	// in that case, our program have nothing to do and should return.
+	if cmp.Complete() {
+		return
+	}
+
+	// if the completion did not do anything, we can run our program logic here.
+	if name == "" {
+		fmt.Println("Your name is missing")
+		os.Exit(1)
+	}
+
+	fmt.Println("Hi,", name)
+
+	return
 	cwd, err := os.Getwd()
 	if err != nil {
 		fmt.Println(err)
