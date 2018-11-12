c9823f283e069555797bafaa87927f0883f57d26
Jason Dixon <jason.dixon.email@gmail.com>
2018-11-12 22:11:53 +1300

    more img tests

--- lock/lock_test.go
+++ lock/lock_test.go
@@ -23,29 +23,44 @@ func TestGenerateContentHash(t *testing.T) {
 
 // TODO: test different image types (eg png)
 // TODO: test error on non-image type
-func TestGeneratePercetualHash(t *testing.T) {
+func testgeneratepercetualhash(t *testing.T) {
 	tu := testutil.NewTestUtil(t)
 	defer tu.LoadTestdata()()
 
-	handle := tu.MustFatal(os.Open(filepath.Join(tu.Dir, "testimg.jpg"))).(*os.File)
-	defer handle.Close()
+	handle1 := tu.MustFatal(os.Open(filepath.Join(tu.Dir, "testimg1.jpg"))).(*os.File)
+	defer handle1.Close()
+	handle2 := tu.MustFatal(os.Open(filepath.Join(tu.Dir, "testimg2.jpg"))).(*os.File)
+	defer handle2.Close()
+	handle3 := tu.MustFatal(os.Open(filepath.Join(tu.Dir, "testimg3.jpg"))).(*os.File)
+	defer handle3.Close()
+	handle4 := tu.MustFatal(os.Open(filepath.Join(tu.Dir, "testimg4.jpg"))).(*os.File)
+	defer handle4.Close()
+	handle5 := tu.MustFatal(os.Open(filepath.Join(tu.Dir, "testimg5.png"))).(*os.File)
+	defer handle5.Close()
+	handle6 := tu.MustFatal(os.Open(filepath.Join(tu.Dir, "testimg6.txt"))).(*os.File)
+	defer handle6.Close()
 
-	expectHash := "a:00070f0f7f1f0703"
-	if !tu.Must(IsSamePerceptualHash(expectHash, expectHash)).(bool) {
-		tu.Fail("Well hash comparison failed...")
+	testHash1 := tu.Must(GeneratePerceptualHash("average", handle1)).(string)
+	testHash2 := tu.Must(GeneratePerceptualHash("average", handle2)).(string)
+	testHash3 := tu.Must(GeneratePerceptualHash("average", handle3)).(string)
+	testHash4 := tu.Must(GeneratePerceptualHash("average", handle4)).(string)
+	_, err := GeneratePerceptualHash("average", handle5)
+	if err == nil {
+		tu.Fail("Allowed unsupported img")
 	}
-	if tu.Must(IsSamePerceptualHash(expectHash, "a:00070f0f7f1f1233")).(bool) {
-		tu.Fail("False positive hash comparison")
-	}
-	if _, err := IsSamePerceptualHash("am I a hash?", expectHash); err == nil {
-		tu.Fail("Succeeded on bad first argument")
-	}
-	if _, err := IsSamePerceptualHash(expectHash, "am I a hash?"); err == nil {
-		tu.Fail("Succeeded on bad second argument")
+	_, err = GeneratePerceptualHash("average", handle6)
+	if err == nil {
+		tu.Fail("Allowed unsupported file")
 	}
 
-	testHash := tu.Must(GeneratePerceptualHash("average", handle)).(string)
-	if !tu.Must(IsSamePerceptualHash(expectHash, testHash)).(bool) {
-		tu.FailE(expectHash, testHash)
+	if !tu.Must(IsSamePerceptualHash(testHash1, testHash2)).(bool) ||
+		!tu.Must(IsSamePerceptualHash(testHash1, testHash3)).(bool) ||
+		!tu.Must(IsSamePerceptualHash(testHash2, testHash3)).(bool) {
+		tu.Fail("Equals not equal")
 	}
+
+	if tu.Must(IsSamePerceptualHash(testHash1, testHash4)).(bool) {
+		tu.Fail("False positive")
+	}
+
 }
